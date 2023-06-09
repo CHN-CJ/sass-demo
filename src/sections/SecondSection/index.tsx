8b24092abee603e7b251115f515e85e2b875c1b7
CHN-CJ <2873183291@qq.com>
2023-06-09 15:43:31 +0800

    tab高亮
    引入classnames，实现tab高亮

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,8 +1,65 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import CartoonImage from '../../assets/cartoon.jpg';
+import MovieImage from '../../assets/movie.png';
+import LifeImage from '../../assets/life.jpg';
+import FoodImage from '../../assets/food.jpg';
+import styles from './styles.module.scss';
+import classNames from 'classnames';
+
+const tabs = [
+    {
+        key: 'cartoon',
+        title: '动画'
+    },
+    {
+        key: 'food',
+        title: '美食'
+    },
+    {
+        key: 'movie',
+        title: '电影'
+    },
+    {
+        key: 'life',
+        title: '生活'
+    }
+]
+
+
 const SecondSection: FC = () => {
+
+    const [activeTab, setActiveTab] = useState<string>('cartoon');
+
     return (
-        <div>
-            Tabs + Tab Content
+        <div className={styles.secondSection}>
+            <ul>
+                {tabs.map(tab => (
+                    <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+                        <span>{tab.title}</span>
+                        {/* {activeTab === tab.key && <span className={styles.line}></span>} */}
+                        <span className={classNames(styles.line, { [styles.visible]: activeTab === tab.key })}></span>
+                    </li>
+                ))}
+            </ul>
+
+            <div>
+                <section>
+                    <h2>动画</h2>
+                    <img src={CartoonImage} alt="cartoon" />
+                </section>
+                <section>
+                    <h2>美食</h2>
+                    <img src={FoodImage} alt="food" />
+                </section>
+                <section>
+                    <h2>电影</h2>
+                    <img src={MovieImage} alt="movie" />
+                </section>
+                <section>
+                    <h2>生活</h2>
+                    <img src={LifeImage} alt="life" />
+                </section>
+            </div>
         </div>
     )
 }
